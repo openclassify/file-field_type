0f9c719d4a7823f11f6f1898e53fc5c9f6b9ae35
Ryan Thompson <service@aiwebsystems.com>
2014-10-22 13:34:29 -0500

    Clean up before final

--- src/FileFieldType.php
+++ src/FileFieldType.php
@@ -4,100 +4,17 @@ use Anomaly\Streams\Platform\Addon\FieldType\FieldTypeAddon;
 
 class FileFieldType extends FieldTypeAddon
 {
-    protected $slug = 'file';
-
-    /**
-     * The database column type this field type uses.
-     *
-     * @var string
-     */
-    public $columnType = 'string';
-
-    /**
-     * Field type version
-     *
-     * @var string
-     */
-    public $version = '1.1.0';
-
-    /**
-     * Available field type settings.
-     *
-     * @var array
-     */
     public $settings = array(
         'allowedFileTypes',
     );
 
-    /**
-     * Field type author information.
-     *
-     * @var array
-     */
-    public $author = array(
-        'name' => 'AI Web Systems, Inc.',
-        'url'  => 'http://aiwebsystems.com/',
-    );
-
-    /**
-     * The field type relation
-     *
-     * @return object
-     */
     public function relation()
     {
         return $this->belongsTo($this->getParameter('relation_class', 'Files\Model\File'));
     }
 
-    /**
-     * Return the input used for forms.
-     *
-     * @return mixed
-     */
-    public function formInput()
+    public function input()
     {
         return \Form::file($this->formSlug);
-    }
-
-    /**
-     * Process value before saving.
-     *
-     * @return mixed
-     */
-    public function preSave()
-    {
-        if (\Input::hasFile($this->formSlug)) {
-            $file = \Input::file($this->formSlug);
-        }
-    }
-
-    /**
-     * Return the string output value.
-     *
-     * @return string
-     */
-    public function stringOutput()
-    {
-        // @todo - Return a string input once more complete
-    }
-
-    /**
-     * Return the plugin output value.
-     *
-     * @return string
-     */
-    public function pluginOutput()
-    {
-        // @todo - Return a plugin input once more complete
-    }
-
-    /**
-     * Get database column name.
-     *
-     * @return string
-     */
-    public function getColumnName()
-    {
-        return parent::getColumnName() . '_id';
     }
 }
