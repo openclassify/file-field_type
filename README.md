f51b6e33aa82bfd59f3b7fe995d12e74ada0b2b8
Ryan Thompson <service@aiwebsystems.com>
2015-04-09 00:33:18 -0500

    Progress

---
+++ README.md
@@ -0,0 +1,3 @@
+# File Field Type
+
+*anomaly.field_type.file*
--- src/FileFieldType.php
+++ src/FileFieldType.php
@@ -2,11 +2,22 @@
 
 use Anomaly\Streams\Platform\Addon\FieldType\FieldType;
 
+/**
+ * Class FileFieldType
+ *
+ * @link          http://anomaly.is/streams-platform
+ * @author        AnomalyLabs, Inc. <hello@anomaly.is>
+ * @author        Ryan Thompson <ryan@anomaly.is>
+ * @package       Anomaly\FileFieldType
+ */
 class FileFieldType extends FieldType
 {
 
-    public function getRelation()
-    {
-        return $this->belongsTo(array_get($this->config, 'related', 'Files\Model\File'));
-    }
+    /**
+     * The input view.
+     *
+     * @var string
+     */
+    protected $inputView = 'anomaly.field_type.file::input';
+
 }
