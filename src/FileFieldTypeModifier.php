ade9e160b5b77e241448744181b4c271024fbc74
Ryan Thompson <service@aiwebsystems.com>
2015-12-10 02:32:50 -0600

    Optimization

--- src/FileFieldTypeModifier.php
+++ src/FileFieldTypeModifier.php
@@ -50,23 +50,4 @@ class FileFieldTypeModifier extends FieldTypeModifier
 
         return null;
     }
-
-    /**
-     * Restore the value from storage format.
-     *
-     * @param  $value
-     * @return null|FileInterface
-     */
-    public function restore($value)
-    {
-        if ($value instanceof FileInterface) {
-            return $value;
-        }
-
-        if ($value && $file = $this->files->find($value)) {
-            return $file;
-        }
-
-        return null;
-    }
 }
