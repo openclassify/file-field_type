b207d871111a77ab6c6850b4977c298176779acd
Ryan Thompson <service@aiwebsystems.com>
2015-12-21 18:43:33 -0600

    Fixed dumb idea for presenters

--- src/FileFieldTypePresenter.php
+++ src/FileFieldTypePresenter.php
@@ -1,8 +1,8 @@
 <?php namespace Anomaly\FileFieldType;
 
 use Anomaly\FilesModule\File\Contract\FileInterface;
+use Anomaly\FilesModule\File\FilePresenter;
 use Anomaly\Streams\Platform\Addon\FieldType\FieldTypePresenter;
-use Anomaly\Streams\Platform\Support\Decorator;
 
 /**
  * Class FileFieldTypePresenter
@@ -27,51 +27,9 @@ class FileFieldTypePresenter extends FieldTypePresenter
             return null;
         }
 
-        return (new Decorator())->decorate($file)->preview();
-    }
+        /* @var FilePresenter $presenter */
+        $presenter = $this->__getDecorator()->decorate($file);
 
-    /**
-     * Fallback to getting attributes
-     * off the related value.
-     *
-     * @param string $key
-     * @return mixed
-     */
-    public function __get($key)
-    {
-        if ($return = parent::__get($key)) {
-            return $return;
-        }
-
-        if ($related = $this->object->getValue()) {
-            if ($return = self::__getDecorator()->decorate($related)->{$key}) {
-                return $return;
-            }
-        }
-
-        return null;
-    }
-
-    /**
-     * Fallback to calling methods
-     * on the related value.
-     *
-     * @param string $method
-     * @param array  $arguments
-     * @return mixed
-     */
-    public function __call($method, $arguments)
-    {
-        if ($return = parent::__call($method, $arguments)) {
-            return $return;
-        }
-
-        if ($related = $this->object->getValue()) {
-            if ($return = call_user_func_array([self::__getDecorator()->decorate($related), $method], $arguments)) {
-                return $return;
-            }
-        }
-
-        return null;
+        return $presenter->preview();
     }
 }
