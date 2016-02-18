6120db6ef9e8e7c08471c0db8f84767fc9185d09
Ryan Thompson <ryan@pyrocms.com>
2016-02-18 01:43:51 -0600

    Author

--- src/FileFieldType.php
+++ src/FileFieldType.php
@@ -9,9 +9,9 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 /**
  * Class FileFieldType
  *
- * @link          http://anomaly.is/streams-platform
- * @author        AnomalyLabs, Inc. <hello@anomaly.is>
- * @author        Ryan Thompson <ryan@anomaly.is>
+ * @link          http://pyrocms.com/
+ * @author        PyroCMS, Inc. <support@pyrocms.com>
+ * @author        Ryan Thompson <ryan@pyrocms.com>
  * @package       Anomaly\FileFieldType
  */
 class FileFieldType extends FieldType
