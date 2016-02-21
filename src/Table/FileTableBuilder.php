b2a0f884acc0b6935cc5b0b2f9dd0f61092913a5
Ryan Thompson <ryan@pyrocms.com>
2016-02-20 19:30:06 -0600

    Fix tables

--- src/Table/FileTableBuilder.php
+++ src/Table/FileTableBuilder.php
@@ -37,6 +37,39 @@ class FileTableBuilder extends TableBuilder
     protected $model = FileModel::class;
 
     /**
+     * The table columns.
+     *
+     * @var array
+     */
+    protected $columns = [
+        'entry.preview' => [
+            'heading' => 'anomaly.module.files::field.preview.name'
+        ],
+        'name'          => [
+            'sort_column' => 'name',
+            'wrapper'     => '
+                    <strong>{value.file}</strong>
+                    <br>
+                    <small class="text-muted">{value.disk}://{value.folder}/{value.file}</small>
+                    <br>
+                    <span>{value.size} {value.keywords}</span>',
+            'value'       => [
+                'file'     => 'entry.name',
+                'folder'   => 'entry.folder.slug',
+                'keywords' => 'entry.keywords.labels',
+                'disk'     => 'entry.folder.disk.slug',
+                'size'     => 'entry.size_label'
+            ]
+        ],
+        'size'          => [
+            'sort_column' => 'size',
+            'value'       => 'entry.readable_size'
+        ],
+        'mime_type',
+        'folder'
+    ];
+
+    /**
      * The table buttons.
      *
      * @var array
