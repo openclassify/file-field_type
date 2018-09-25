86e103a5ba3f9fd1deea89c84415e546cc9ec63e
Martin Modler <modler@webformatik.com>
2018-09-25 08:34:40 +0200

    Updated de translation

--- resources/lang/de/config.php
+++ resources/lang/de/config.php
@@ -1,24 +1,23 @@
 <?php
 
 return [
-    'disk'  => [
-        'label'        => 'Upload Disk', // @todo: decide if we want to translate the expression "disk" or leave it as it is
-        'instructions' => 'Wählen Sie eine &laquo;Disk&raquo; in welche die Date hochgeladen wird.',
+    'folders' => [
+        'name'         => 'Folders',
+        'instructions' => 'Specify which folders are available for this field. Leave blank to display all folders.',
+        'warning'      => 'Existing folder permissions take precedence over selected folders.',
     ],
-    'path'  => [
-        'label'        => 'Upload Pfad',
-        'instructions' => 'Geben Sie einen Pfad an, in den die Datei hochgeladen werden soll, oder lassen Sie das Feld leer um die Datei in das Wurzelverzeichnis der Disk hochzuladen.<br>Wenn der Pfad nicht existiert, wird er während dem Upload erstellt.',
+    'max'     => [
+        'name'         => 'Maximale Upload Grösse',
+        'instructions' => 'Geben Sie die maximal zulässige Dateigrösse in <strong>Megabyte</strong> an.',
+        'warning'      => 'Wenn kein Wert angegeben wurde, wird der Maximalwert des Ordners und dann der des Servers verwendet.',
     ],
-    'image' => [
-        'label'        => 'Nur Bilder',
-        'instructions' => 'Soll der Upload auf Bilder beschränkt werden?',
-    ],
-    'mimes' => [
-        'label'        => 'Erlaubte Dateien',
-        'instructions' => 'Geben Sie die Erweiterung der erlaubten Dateien an. Wenn keine Erweiterungen eingegeben werden, ist jeder Dateityp für den Upload erlaubt.',
-    ],
-    'max'   => [
-        'label'        => 'Maximale Grösse',
-        'instructions' => 'Geben Sie die maximal zulässige Dateigrösse in <strong>Megabyte</strong> an.<br>Der Standardwert ist die vom System vorgegebene maximal zulässige Dateigrösse für Uploads.',
+    'mode'    => [
+        'name'         => 'Eingabemodus',
+        'instructions' => 'Wie sollen Benutzer Dateien bereitstellen können?',
+        'option'       => [
+            'default' => 'Upload und/oder Dateiauswahl.',
+            'select'  => 'Nur Dateiauswahl.',
+            'upload'  => 'Nur Upload.',
+        ],
     ],
 ];
